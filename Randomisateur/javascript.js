823ed37cda94145765d928cd51ff75fe45b420e3
slink7 <75044895+slink7@users.noreply.github.com>
2022-03-03 22:45:14 +0100

    Add files via upload

--- Randomisateur/javascript.js
+++ Randomisateur/javascript.js
@@ -16,53 +16,42 @@ var max_value = 10, current_value = 0, previous_value = 0, mean = 0.0;
 var sum = 0, count = 0;
 
 function update_display() {
-    var e = document.getElementById("max-value");
-    e.innerHTML = max_value;
-    e = document.getElementById("current-value");
-    e.innerHTML = current_value;
-    e = document.getElementById("previous-value");
-    e.innerHTML = previous_value;
-    e = document.getElementById("mean-value");
-    e.innerHTML = mean;
-    document.getElementById("commentary").innerHTML = messages[messages.length*(current_value/max_value)];
-    document.getElementById("commentary").style.color = "rgb("+(current_value/max_value)*255+","+(1-current_value/max_value)*255+",0)";
-    document.getElementById("commentary").style.textShadow = "rgb("+(current_value/max_value)*255+","+(1-current_value/max_value)*255+",0) 0 0 10px";
+    document.getElementById("max-value").innerHTML = max_value;
+    document.getElementById("current-value").innerHTML = current_value;
+    document.getElementById("previous-value").innerHTML = previous_value;
+    document.getElementById("mean-value").innerHTML = mean;
+    var prctg = current_value/max_value;
+    document.getElementById("commentary").innerHTML = messages[Math.round((messages.length-1)*prctg)];
+    document.getElementById("commentary").style.color = "rgb("+prctg*255+","+(1-prctg)*255+",0)";
+    document.getElementById("commentary").style.textShadow = "rgb("+prctg*255+","+(1-prctg)*255+",0) 0 0 10px";
 }
 
 window.onload = function() {
-    var btn;
-    btn = document.getElementById("btnm1");
-    btn.onclick = function() {
+    document.getElementById("btnm1").onclick = function() {
         max_value-=1;
         update_display();
     }
-    btn = document.getElementById("btnm10");
-    btn.onclick = function() {
+    document.getElementById("btnm10").onclick = function() {
         max_value-=10;
         update_display();
     }
-    btn = document.getElementById("btnm100");
-    btn.onclick = function() {
+    document.getElementById("btnm100").onclick = function() {
         max_value-=100;
         update_display();
     }
-    btn = document.getElementById("btnp1");
-    btn.onclick = function() {
+    document.getElementById("btnp1").onclick = function() {
         max_value+=1;
         update_display();
     }
-    btn = document.getElementById("btnp10");
-    btn.onclick = function() {
+    document.getElementById("btnp10").onclick = function() {
         max_value+=10;
         update_display();
     }
-    btn = document.getElementById("btnp100");
-    btn.onclick = function() {
+    document.getElementById("btnp100").onclick = function() {
         max_value+=100;
         update_display();
     }
-    btn = document.getElementById("btnGENERATE");
-    btn.onclick = function() {
+    document.getElementById("btnGENERATE").onclick = function() {
         previous_value = current_value;
         current_value = Math.round(Math.random()*max_value);
         sum += current_value;
